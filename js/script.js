827ea186f5917176d5301002c0d36c5f94ee70ce
Ren Ivey <haleyrivey@gmail.com>
2021-06-25 15:32:04 -0400

    Added current date to top of page. Additional styling done.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var userText = document.getElementById(".textarea");
+userText = $('input');
+
+var currentDate = moment().toDate()
+var currentDatePage = document.querySelector(".currentdate");
+currentDatePage.textContent = currentDate; 
+
+// localStorage.setItem(userText.value)
