cf7b4f319859fca891b42bbe517c7d3aafd69cbd
Ren Ivey <haleyrivey@gmail.com>
2021-06-25 20:25:42 -0400

    Targeted each hour column ID in order to change background color of the input row based on current hour.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-var userText = document.getElementById(".textarea");
-userText = $('input');
+// Assigning page elements to variables.
+var userText = $(".textarea");
+var currentDatePage = $(".currentdate");
 
+// Other variables.
 var currentDate = moment().toDate()
-var currentDatePage = document.querySelector(".currentdate");
+var currentHour = moment().format("h")
+
+// Adds current date (and time) to top of page.
 currentDatePage.textContent = currentDate; 
 
+$(".hour").each(function() {
+    var colHour = $(this).attr("id") 
+    console.log("Column ID/hour = ", colHour);
+    console.log("Current hour = ", currentHour);
+        if (currentHour === colHour) {
+        $( ".textarea" ).addClass("present");
+        } else if (currentHour > colHour) {
+        $( ".textarea" ).addClass("past");
+        } else if (currentHour < colHour) {
+        $( ".textarea" ).addClass("future");
+        }
+    }
+)
+
+// userText = $('input');
 // localStorage.setItem(userText.value)
